087070e5872edf6108cc8bfd57a275c13d7b2b75
Luke Olfert <contact@lukeolfert.com>
2020-06-07 15:54:05 -0500

    Changed border color, disabled smart gaps

--- config.def.h
+++ config.def.h
@@ -1,7 +1,7 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 2;        /* border pixel of windows */
+static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const unsigned int gap       = 32;       /* horiz inner gap between windows */
 static const unsigned int gappih    = gap;       /* horiz inner gap between windows */
@@ -9,7 +9,7 @@ static const unsigned int gappiv    = gap;       /* vert inner gap between windo
 static const unsigned int gappoh    = gap;       /* horiz outer gap between windows and screen edge */
 static const unsigned int gappov    = gap;       /* vert outer gap between windows and screen edge */
 static const int sidepad            = gap;       /* horizontal padding for statusbar */
-static const int smartgaps          = 1;        /* 1 means no outer gap when there is only one window */
+static const int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 0;        /* 0 means bottom bar */
 static const int horizpadbar        = 96;       /* horizontal padding for statusbar */
@@ -35,7 +35,7 @@ static const char col_magenta[]     = "#894BA4";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_fg, col_bg, col_bg },
-	[SchemeSel]  = { col_fg_f, col_bg_f, col_br_f },
+	[SchemeSel]  = { col_fg_f, col_bg_f, col_cyan },
 	[SchemeInd]  = { col_bg, col_bg, col_bg },
 	[SchemeInd_f] = { col_magenta, col_magenta, col_magenta }
 };
