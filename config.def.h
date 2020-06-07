136411ba451756724fd6d2c1715f41a25a16c855
Luke Olfert <contact@lukeolfert.com>
2020-06-07 15:23:50 -0500

    Added keybindings, colorschemes, gap defaults

--- config.def.h
+++ config.def.h
@@ -1,39 +1,55 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 1;        /* border pixel of windows */
+static const unsigned int borderpx  = 2;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
-static const unsigned int gappih    = 20;       /* horiz inner gap between windows */
-static const unsigned int gappiv    = 10;       /* vert inner gap between windows */
-static const unsigned int gappoh    = 10;       /* horiz outer gap between windows and screen edge */
-static const unsigned int gappov    = 30;       /* vert outer gap between windows and screen edge */
-static       int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
+static const unsigned int gap       = 32;       /* horiz inner gap between windows */
+static const unsigned int gappih    = gap;       /* horiz inner gap between windows */
+static const unsigned int gappiv    = gap;       /* vert inner gap between windows */
+static const unsigned int gappoh    = gap;       /* horiz outer gap between windows and screen edge */
+static const unsigned int gappov    = gap;       /* vert outer gap between windows and screen edge */
+static const int sidepad            = gap;       /* horizontal padding for statusbar */
+static const int smartgaps          = 1;        /* 1 means no outer gap when there is only one window */
 static const int showbar            = 1;        /* 0 means no bar */
-static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=10" };
-static const char dmenufont[]       = "monospace:size=10";
+static const int topbar             = 0;        /* 0 means bottom bar */
+static const int horizpadbar        = 96;       /* horizontal padding for statusbar */
+static const int vertpadbar         = 36;       /* vertical padding for statusbar */
+static const int indicatorv         = 2;        /* thickness of the indicators in bar */
+static const char *fonts[]          = { "IBM 3270:size=13", "micon:size=13" };
+static const char dmenufont[]       = "IBM 3280:size=10";
+
+static const char col_bg[]          = "#26263A";
+static const char col_bg_f[]        = "#2E2E46";
+static const char col_br_f[]        = "#34344B";
+static const char col_fg[]          = "#bbbbbb";
+static const char col_fg_f[]        = "#eeeeee";
+
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
+
+static const char col_cyan[]        = "#37BDB9";
+static const char col_magenta[]     = "#894BA4";
+
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+	[SchemeNorm] = { col_fg, col_bg, col_bg },
+	[SchemeSel]  = { col_fg_f, col_bg_f, col_br_f },
+	[SchemeInd]  = { col_bg, col_bg, col_bg },
+	[SchemeInd_f] = { col_magenta, col_magenta, col_magenta }
 };
 
 /* tagging */
-static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+static const char *tags[] = { "", "", "", "", "", "", "", "", "" };
 
 static const Rule rules[] = {
 	/* xprop(1):
 	 *	WM_CLASS(STRING) = instance, class
 	 *	WM_NAME(STRING) = title
 	 */
-	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
-	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
+	/* class                 instance     title       tags mask     isfloating   monitor */
+	{ "Gnome-flashback",      NULL,       NULL,       1 << 8,            0,           NULL }
 };
 
 /* layout(s) */
--- dwm.c
+++ dwm.c
@@ -59,7 +59,7 @@
 
 /* enums */
 enum { CurNormal, CurResize, CurMove, CurLast }; /* cursor */
-enum { SchemeNorm, SchemeSel }; /* color schemes */
+enum { SchemeNorm, SchemeSel, SchemeInd, SchemeInd_f }; /* color schemes */
 enum { NetSupported, NetWMName, NetWMState, NetWMCheck,
        NetWMFullscreen, NetActiveWindow, NetWMWindowType,
        NetWMWindowTypeDialog, NetClientList, NetLast }; /* EWMH atoms */
